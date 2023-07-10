c7d58b0734cab3f0b9df985fb5b78788472f57f0
Magomed <prosto.drom@mail.ru>
2023-07-10 23:42:41 +0300

    Almost done

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+using std::cin, std::cout, std::endl;
+
+void calculate(int num_1, int num_2){
+
+}
+
+int  main(){
+    cout << "Enter two numbers" << endl;
+    
+    int num_1, num_2;
+    cin >> num_1 >> num_2;
+
+    return 0;
+}
