43f01a459ea9b6956d06112d2f88af38d952b283
Magomed <prosto.drom@mail.ru>
2023-07-11 00:22:03 +0300

    Done?

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using std::cin, std::cout, std::endl;
 
-void calculate(int num_1, int num_2){
+void calculate(int num_1, int num_2, char op){
+    int res;
+    
+    if (op == '+'){
+        res = num_1 + num_2;
+    }
+    else if (op == '-'){
+        res = num_1 - num_2;
+    }
+    else if (op == '*')
+    {
+        res = num_1 * num_2;
+    }
+        else if (op == '/')
+    {
+        res = num_1 / num_2;
+    }
 
+    cout << res;
 }
 
 int  main(){
@@ -10,6 +27,13 @@ int  main(){
     
     int num_1, num_2;
     cin >> num_1 >> num_2;
+
+    cout << "Enter operator you want to calculate with: '+', '-', '*', '/'" << endl;
+
+    char op;
+    cin >> op;
+
+    calculate(num_1, num_2, op);
 
     return 0;
 }
